37a77945c4971e7ecbfb5f96197c87ba7337693b
Rajesh Hegde <rajeshhegde180@gmail.com>
2020-09-22 22:19:37 +0530

    added files

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,12 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <v-app-bar dark>
+      <v-spacer />
+      <v-toolbar-title>Bhajana Suma | ಭಜನ ಸುಮ</v-toolbar-title>
+      <v-spacer />
+    </v-app-bar>
+    <router-view />
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
